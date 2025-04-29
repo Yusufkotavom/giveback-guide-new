38aa19b12952bfa8784a0d894fba4f77cf71eaf2
Matt Morelli <mmorelli@gmail.com>
2025-04-29 11:11:02 +0100

    Added server rendering and Netlify adapter for search functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://giveback.guide',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
