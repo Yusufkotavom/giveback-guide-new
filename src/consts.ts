f88a4768a322f7033d55fa40fa05486f11c316b3
Matt Morelli <mmorelli@gmail.com>
2025-06-18 14:21:20 +0100

    Multiple tweaks, too many to remember, sorry!

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Giveback';
+export const SITE_TITLE = 'Giveback Guide';
 export const SITE_DESCRIPTION = 'Discover amazing ways to give back to the places you visit.';
